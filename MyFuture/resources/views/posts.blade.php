e78522c76df9e05c28856ed618f8f020b51cfa9e
Juanba Perez <Juanba58@hotmail.com>
2019-09-16 13:00:59 -0300

    getting vendors folder

--- MyFuture/resources/views/posts.blade.php
+++ MyFuture/resources/views/posts.blade.php
@@ -19,37 +19,38 @@ Posts
             <img class="profilePicture" src="{{asset('storage/mJG1PzYLEjNedOGHUIjvxyGqpoc3vXxLPWdVlv8i.png')}}">
 
             <div class=" userName">
-            <h2>{{$post->user["userName"]}}</h2>
-            <p>30 minutes</p>
-        </div>
-
-    </div>
-
-    <div class="postImage">
-        <img style="width: 100%" src="{{asset('storage/posts/LvG675j6XkyP2bNNFWEXOSM2J5gayKHyLxSC8o7f.jpeg')}}">
-    </div>
-
-    <div class="description">
-        <p><a style="color: black; font-size: 30px" href="/post/{{$post->id}}">
-                {{$post->description}}
-            </a></p>
-    </div>
-
-
-    <div class="postIcons">
-
-        <div class="postIcons-heart">
-            <button class="buttons" type="button" name="button"><i class="fa fa-heart"> 30</i></button>
+                <h2>{{$post->user["userName"]}}</h2>
+                <p>30 minutes</p>
+            </div>
 
         </div>
 
-
-        <div class="postIcons-comment">
-
-            <button class="buttons" type="button" name="button"><i class="fa fa-comment"> 12</i></button>
-
+        <div class="postImage">
+            {{dd($post->user())}}
+            <img style="width: 100%" src="{{asset('storage/posts/LvG675j6XkyP2bNNFWEXOSM2J5gayKHyLxSC8o7f.jpeg')}}">
         </div>
-    </div>
+
+        <div class="description">
+            <p><a style="color: black; font-size: 30px" href="/post/{{$post->id}}">
+                    {{$post->description}}
+                </a></p>
+        </div>
+
+
+        <div class="postIcons">
+
+            <div class="postIcons-heart">
+                <button class="buttons" type="button" name="button"><i class="fa fa-heart"> 30</i></button>
+
+            </div>
+
+
+            <div class="postIcons-comment">
+
+                <button class="buttons" type="button" name="button"><i class="fa fa-comment"> 12</i></button>
+
+            </div>
+        </div>
 
 
 
@@ -72,5 +73,3 @@ Posts
 
 
 @endsection
-
-
